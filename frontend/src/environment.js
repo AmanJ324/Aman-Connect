9635f720c6a023dbd1fa61d34832c4b3f9b24dfc
AmanJ324 <amanjain9461@gmail.com>
2026-03-24 21:37:58 +0530

    deploy

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://dhamsa-call.onrender.com" :
+    "https://aman-connect.onrender.com" :
 
     "http://localhost:8000"
 
